f7fa641ed4614277c579d722ed2014372863599f
Nick Hendricks <hen17013@byui.edu>
2020-01-18 23:34:38 -0700

    First Fluency 1.2

--- assignments/fluency01/script.js
+++ assignments/fluency01/script.js
@@ -1,8 +1,8 @@
 function selectNumber(num){
     for (var i = 0; i < num; i++) {
-        document.getElementsByClassName("centerCol").appendChild("This is via a loop. " + (num - i) + " more times." + "<br>");
+        document.getElementsByClassName("centerCol").innerHTML += ("This is via a loop. " + (num - i) + " more times." + "<br>");
       }
-      document.getElementsByClassName("centerCol").appendChild("Complete. <br>");
+      document.getElementsByClassName("centerCol").innerHTML += ("Complete. <br>");
 
       if(num % 2 == 0){
         document.getElementsByClassName("centerCol").style.color = "blue";
