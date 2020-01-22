f4065bd834c5766033b4cfcdd1d3fb2d3ab7c528
Nick Hendricks <hen17013@byui.edu>
2020-01-22 13:17:10 -0700

    First Fluency - Global Array and Function

--- assignments/fluency01/script.js
+++ assignments/fluency01/script.js
@@ -1,13 +1,30 @@
+// Global array
+var numArray;
+
+// Function
 function selectNumber(num){
+    // push num onto array.
+    numArray.push(num);
+
+    // Loops
     for (var i = 0; i < num; i++) {
         document.getElementById("centerText").innerHTML += ("This is via a loop. " + (num - (i + 1)) + " more times." + "<br>");
       }
-        document.getElementById("centerText").innerHTML += ("Complete. <br>");
-
-      if(num % 2 == 0){
+    
+    document.getElementById("centerText").innerHTML += ("Complete. <br>");
+     
+    // Conditional Statement
+    if(num % 2 == 0){
         document.getElementById("centerText").style.color = "blue";
-      }
-      else{
+    }
+    else{
         document.getElementById("centerText").style.color = "red";
-      }
+    }
+}
+
+// Displays array in third column.
+function displayArray(){
+    for (var i = 0; i < numArray.length(); i++){
+        document.getElementById("rightText").innerHTML += numArray[i] + "<br>";
+    }
 }
