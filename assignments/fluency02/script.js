49f06c4f3c99e700ace196702423f90f8da1f68d
Nick Hendricks <hen17013@byui.edu>
2020-01-25 00:30:38 -0700

    Fluency 02 - 2.9 - Loop through array

--- assignments/fluency02/script.js
+++ assignments/fluency02/script.js
@@ -1,5 +1,6 @@
 // JavaScript Objects - Object Creation Functions, Inheritance, Properties, Methods, Instantiation 
 
+// default class
 class Person {
     constructor(first, last, age, gender) {
         this.name = {
@@ -18,24 +19,29 @@ class Student extends Person{
         this.grade = grade;
     }
 
+    // getter for profile data
     getProfile(){
         return ("Student <br>" + 
                 "Name: " + this.name.first + " " + this.name.last + "<br>" +
                 "Grade: " + this.grade + "<br>" +
                 "Age: " + this.age + "<br>" +
-                "Gender: " + this.gender + "<br>");
+                "Gender: " + this.gender + "<br><br>");
     }
 }
 
 // array of students
 var listofStudents = [];
 
+// calls constructor to create object, adds to array.
 function createProfile(first, last, grade, age, gender){
     listofStudents.push(new Student(first, last, grade, age, gender));
     console.log("Array: " + listofStudents);
     console.log("Array Length: " + listofStudents.length);
 }
 
+// loops through array to call profiles
 function requestProfile(){
-    document.getElementById("classProfiles").innerHTML += listofStudents[0].getProfile();
+    for (var i = 0; i < listofStudents.length; i++){    
+        document.getElementById("classProfiles").innerHTML += listofStudents[i].getProfile();
+    }
 }
