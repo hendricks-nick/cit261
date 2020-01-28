eb4965ae3008cc953ead1dac7afb7544961c3ab7
Nick Hendricks <hen17013@byui.edu>
2020-01-28 11:52:24 -0700

    AJAX Setup 1.1 - update form action

--- assignments/fluency04/script.js
+++ assignments/fluency04/script.js
@@ -10,8 +10,11 @@ function weatherRequest(urlRequest) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-       //document.getElementById("demo").innerHTML = this.responseText;
-       console.log(this.responseText);
+          //document.getElementById("demo").innerHTML = this.responseText;
+          console.log(this.responseText);
+      }
+      else {
+          console.log(this.responseText);
       }
     };
     xhttp.open("GET", urlRequest , true);
