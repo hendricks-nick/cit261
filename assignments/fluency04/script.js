8da2b27131dd1502fd922006b0da154dbf6b7807
Nick Hendricks <hen17013@byui.edu>
2020-01-28 11:24:54 -0700

    AJAX Setup

---
+++ assignments/fluency04/script.js
@@ -0,0 +1,21 @@
+function getWeather(zip, country){
+    const apiKey = "081ff0cf85a3a31024f8fac0a6142051";
+
+    var urlRequest = "api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + country + "&APPID=" + apiKey;
+
+    weatherRequest(urlRequest);
+}
+
+function weatherRequest(urlRequest) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       //document.getElementById("demo").innerHTML = this.responseText;
+       console.log(this.responseText);
+      }
+    };
+    xhttp.open("GET", urlRequest , true);
+    xhttp.send();
+  }
+
+  // form > getWeather > make request > update HTML
