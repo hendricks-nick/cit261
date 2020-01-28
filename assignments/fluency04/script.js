a46f592ce25039870f4c36fff56bc54bcee98007
Nick Hendricks <hendricksn@byui.edu>
2020-01-28 13:48:11 -0700

    test ajax 3

--- assignments/fluency04/script.js
+++ assignments/fluency04/script.js
@@ -11,22 +11,11 @@ var HttpClient = function() {
     }
 }
 
-$(document).ready(function () {
-    $("#submitBtn").click(function () {
-        var formData = $("#contactForm").serialize();
-        $.ajax({
-            type: "GET",
-            url: 'api.openweathermap.org/data/2.5/weather?zip=85210&APPID=081ff0cf85a3a31024f8fac0a6142051', //serverside
-            beforeSend: function () {
-                //show loading image
-            },
-            success: function (result) {
-                console.log(result); //use this to see the response from serverside
-            },
-            error: function (e) {
-                console.log(e); //use this to see an error in ajax request
-            }
-        });
+$('.weatherForm').on('submit', function () {
+    var client = new HttpClient();
+    console.log(urlRequest);
+    client.get('api.openweathermap.org/data/2.5/weather?zip=85210&APPID=081ff0cf85a3a31024f8fac0a6142051', function(response) {
+        console.log(response);
     });
 });
 
