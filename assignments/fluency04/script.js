5ca502a97a8efad71ca35d576984b7fe5bd7a0b4
Nick Hendricks <hen17013@byui.edu>
2020-01-28 18:38:01 -0700

    AJAX finish 3

--- assignments/fluency04/script.js
+++ assignments/fluency04/script.js
@@ -1,4 +1,4 @@
-var HttpClient = function() {
+/*var HttpClient = function() {
     this.get = function(aUrl, aCallback) {
         var anHttpRequest = new XMLHttpRequest();
         anHttpRequest.onreadystatechange = function() { 
@@ -9,24 +9,39 @@ var HttpClient = function() {
         anHttpRequest.open( "GET", aUrl, true );            
         anHttpRequest.send();
     }
-}
+}*/
 
 function getWeather(city, country){
     const apiKey = "081ff0cf85a3a31024f8fac0a6142051";
 
     var urlRequest = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=081ff0cf85a3a31024f8fac0a6142051";
-
-    console.log(urlRequest);
     weatherRequest(urlRequest);
 }
 
+/*
 function weatherRequest(urlRequest) {
     var client = new HttpClient();
     console.log(urlRequest);
     client.get(urlRequest, function(response) {
         console.log(response);
     });
-}
+}*/
 
 
+function weatherRequest(urlRequest) {
+
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       console.log(this.responseText);
+
+       var obj = JSON.parse(this.responseText);
+
+       console.log(obj);
+      }
+    };
+    xhttp.open("GET", urlRequest, true);
+    xhttp.send();
+  }
   // form > getWeather > make request > update HTML
