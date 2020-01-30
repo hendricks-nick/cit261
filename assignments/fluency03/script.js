f1c20de80b9c7a50d2117abac1dbd7d8cb73cd99
Nick Hendricks <hendricksn@byui.edu>
2020-01-29 17:23:54 -0700

    json update 4

--- assignments/fluency03/script.js
+++ assignments/fluency03/script.js
@@ -4,9 +4,9 @@ function getJSON() {
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
           console.log(this.responseText);
-          document.getElementById("leftCol").innerHTML = this.responseText;
+          document.getElementById("leftCol").innerHTML = "JSON File: <br> <br>" + this.responseText;
           var obj = JSON.parse(this.responseText);
-          document.getElementById("centerCol").innerHTML = obj;
+          document.getElementById("centerCol").innerHTML = "Parsed: <br> <br>" + obj.fruit + " <br>" + obj.size + " <br>" + obj.color + " <br>";
       }
     };
     xhttp.open("GET", "json.txt", true);
