b4bb0d4dc7e3aff4e347acad4cf3a738e5e877c7
Nick Hendricks <hen17013@byui.edu>
2020-02-19 00:30:14 -0700

    Update

---
+++ assignments/fluency05/script.js
@@ -0,0 +1,11 @@
+class Company{
+    constructor(name, numEmployees, type){
+        this.name = name;
+        this.numEmployees = numEmployees;
+        this.type = type;
+    }
+}
+
+function storeLocally() {
+    
+}
