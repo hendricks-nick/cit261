8fb8117d252bf456a8229bacaf36c659c5e6613a
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2020-02-20 12:07:07 -0700

    Update

--- assignments/fluency08/script.js
+++ assignments/fluency08/script.js
@@ -1,48 +1,16 @@
-// 3x3 grid, when one is clicked, it empties it and fills the ones next to it.
-let myMap = new Map([
-    [1, '0'], 
-    [2, '0'],
-    [3, '0'],
-    [4, '0'],
-    [5, '0'],
-    [6, '0'],
-    [7, '0'],
-    [8, '0'],
-    [9, '0'],
-]);
+var boxOne = document.getElementsByClassName('box')[0],
+    $boxTwo = $('.box:eq(1)');
 
-// Generates 3 random squares
-function startGame() {
-    for (var i = 0; i < 3; i++) {
-        var divNum = Math.floor(Math.random() * 9) + 1;
-        loadSquare(divNum);
-        console.log(divNum);
-    }
-}
-
-function checkSquare(divNum) {
-    if (myMap.get(divNum) === '1'){
-        shrinkSquare(divNum);
-    }
-}
-
-function loadSquare(divNum){
-    myMap.set(divNum, '1');
-    document.getElementById("cell" + divNum).style.width = '100px';
-    document.getElementById("cell" + divNum).style.height = '100px';
-}
-
-function shrinkSquare(divNum) {
-    myMap.set(divNum, '0');
-    document.getElementById("cell" + divNum).style.width = '0px';
-    document.getElementById("cell" + divNum).style.height = '0px';
-
-}
-
-function checkConditions(){
-    for (var i = 0; i < myMap.size; i++) {
-        if (myMap.has(1)){
-            break;
-        }
-    }
+document.getElementsByClassName('toggleButton')[0].onclick = function() {
+  if(this.innerHTML === 'Play') 
+  { 
+    this.innerHTML = 'Pause';
+    boxOne.classList.add('horizTranslate');
+  } else {
+    this.innerHTML = 'Play';
+    var computedStyle = window.getComputedStyle(boxOne),
+        marginLeft = computedStyle.getPropertyValue('margin-left');
+    boxOne.style.marginLeft = marginLeft;
+    boxOne.classList.remove('horizTranslate');    
+  }  
 }
---
+++ assignments/fluency11/script.js
@@ -0,0 +1,48 @@
+// 3x3 grid, when one is clicked, it empties it and fills the ones next to it.
+let myMap = new Map([
+    [1, '0'], 
+    [2, '0'],
+    [3, '0'],
+    [4, '0'],
+    [5, '0'],
+    [6, '0'],
+    [7, '0'],
+    [8, '0'],
+    [9, '0'],
+]);
+
+// Generates 3 random squares
+function startGame() {
+    for (var i = 0; i < 3; i++) {
+        var divNum = Math.floor(Math.random() * 9) + 1;
+        loadSquare(divNum);
+        console.log(divNum);
+    }
+}
+
+function checkSquare(divNum) {
+    if (myMap.get(divNum) === '1'){
+        shrinkSquare(divNum);
+    }
+}
+
+function loadSquare(divNum){
+    myMap.set(divNum, '1');
+    document.getElementById("cell" + divNum).style.width = '100px';
+    document.getElementById("cell" + divNum).style.height = '100px';
+}
+
+function shrinkSquare(divNum) {
+    myMap.set(divNum, '0');
+    document.getElementById("cell" + divNum).style.width = '0px';
+    document.getElementById("cell" + divNum).style.height = '0px';
+
+}
+
+function checkConditions(){
+    for (var i = 0; i < myMap.size; i++) {
+        if (myMap.has('1')){
+            break;
+        }
+    }
+}
