41d20a13084fa7bc2f311ae0b2d25a729711fd47
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2020-02-25 15:53:59 -0700

    CSS Update

--- assignments/fluency07/script.js
+++ assignments/fluency07/script.js
@@ -1,10 +1,23 @@
+
+// change CSS class properties using querySelector and querySelectorAll
 function manipulateStyle(back, front) {
     var docClass = document.querySelector('.bodyStyle');
     docClass.style.backgroundColor = back;
     docClass.style.color = front;
+
+    console.log(docClass);
 }
 
+// change CSS class properties using getElemensByClassName
 function changeFont(font) {
     var docClass = document.getElementsByClassName('bodyStyle');
     docClass[0].style.fontFamily = font;
+    
+    console.log(docClass);
 } 
+
+/* getElemensByClassName and querySelectAll create an array of elements,
+ * when you need to edit ALL of them, a simple for loop will do the trick.
+ *
+ * for (var i = 0; i < docClass.length; i++) { ..loop here.. }
+ */
