0054411bec678c4a7f0493186d0290b036930eec
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2020-02-25 15:36:24 -0700

    Change from ID to Class

--- assignments/fluency07/script.js
+++ assignments/fluency07/script.js
@@ -1,10 +1,10 @@
 function manipulateStyle(back, front) {
-    let docClass = document.getElementsByClassName('bodyStyle');
+    var docClass = document.getElementsByClassName('bodyStyle');
     docClass.style.backgroundColor = back;
     docClass.style.color = front;
 }
 
 function changeFont(font) {
-    let docClass = document.getElementsByClassName('bodyStyle');
+    var docClass = document.getElementsByClassName('bodyStyle');
     docClass.style.font = font;
 } 
