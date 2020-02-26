3b74a583b179c654a4f6e923df55886d7f823c5f
Nick Hendricks <hendricksn@byui.edu>
2020-02-25 20:18:07 -0700

    JS Change

--- assignments/fluency08/script.js
+++ assignments/fluency08/script.js
@@ -1,15 +1,27 @@
 var boxOne = document.getElementsByClassName('box')[0];
 
-document.getElementsByClassName('toggleButton')[0].onclick = function() {
-  if(this.innerHTML === 'Play') 
-  { 
+/* Uses getElementsByClassName to trigger on click event.
+   Checks for play or paused state */
+ document.getElementsByClassName('toggleButton')[0].onclick = function() {
+  if (this.innerHTML === 'Play') { 
     this.innerHTML = 'Pause';
+
+    /* Uses classList, a DOM object function made access to by creating the object above.
+     This changes the class name in the HTML to trigger the transition */
     boxOne.classList.add('horizTranslate');
-  } else {
+  } 
+  else {
     this.innerHTML = 'Play';
-    var computedStyle = window.getComputedStyle(boxOne),
-        marginLeft = computedStyle.getPropertyValue('margin-left');
-    boxOne.style.marginLeft = marginLeft;
+
+    /* Saves the location of the margin left and saves it to the CSS inline 
+       To prevent it from resetting */
+    //var computedStyle = window.getComputedStyle(boxOne),
+        // marginLeft = computedStyle.getPropertyValue('margin-left');
+    // boxOne.style.marginLeft = marginLeft;
     boxOne.classList.remove('horizTranslate');    
   }  
+}
+
+function pauseAnimation() {
+
 }
