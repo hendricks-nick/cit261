db6bd0cd4030906642a6742f8fe6877b22c29df1
Nick Hendricks <hendricksn@byui.edu>
2020-03-10 10:37:06 -0600

    fluency 6 update

---
+++ assignments/fluency06/script.js
@@ -0,0 +1,38 @@
+/*
+Create buttons will create and append the element to their respective column.
+dupRow will give each row a button the option to insertBefore with a different count
+*/
+
+// Create buttons
+function createButton() {
+    var btn = document.createElement("BUTTON");   // Create a <button> element
+    btn.innerHTML = "Insert Before";                   // Insert text
+    document.getElementById("btnColumn").appendChild(btn); 
+}
+
+// Global to keep track of num of paragraphs for dynamic text
+var parNum = 1;
+
+// Create paragraph
+function createParagraph() {
+    var para = document.createElement("P");
+    para.innerText = "This is paragraph number" + parNum;               
+    document.getElementById('parColumn').appendChild(para); 
+
+    parNum++;
+}
+
+// Create text box
+function createText() {
+
+}
+
+// Delete Row
+function deleteRow(id) {
+
+}
+
+// Duplicate Row (insertBefore)
+function dupeRow(row) {
+
+}
