0ab1669ccbd847cfdb704c770558c50bcad3f01d
Nick Hendricks <hen17013@byui.edu>
2020-03-10 19:04:47 -0600

    Fluency 13 - 7

--- assignments/fluency13/script.js
+++ assignments/fluency13/script.js
@@ -1,7 +1,8 @@
 function changeState (className) {
 
+    console.log(document.getElementById(className).style.animationPlayState);
+    
     if (document.getElementById(className).style.animationPlayState == 'paused'){
-        console.log(document.getElementById(className).style.animationPlayState);
         console.log("Switching " + className + " from paused to play.");
         document.getElementById(className).style.animationPlayState = 'running';
         document.getElementById(className + "btn").innerHTML = "Pause";
@@ -11,5 +12,6 @@ function changeState (className) {
         console.log("Switching " + className + " from play to paused.");
         document.getElementById(className).style.animationPlayState = 'paused';
         document.getElementById(className + "btn").innerHTML = "Play";
+        
     }
 }
