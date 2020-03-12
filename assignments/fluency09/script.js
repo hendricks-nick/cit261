01c25a35c2bf24ba1f0e3dc7424a6741eda1ee4f
Nick Hendricks <hendricksn@byui.edu>
2020-03-12 15:20:37 -0600

    Fluency 09-5

--- assignments/fluency09/script.js
+++ assignments/fluency09/script.js
@@ -1,3 +1,13 @@
 function changeDiv(divObj) {
     document.getElementById(divObj.id).style.backgroundColor = 'red';
 }
+
+function changeBack() {
+    document.body.style.backgroundColor = 'cyan';
+
+}
+
+function changeDrop(dropObj) {
+    document.getElementById(dropObj.id).style.backgroundColor = dropObj.value;
+
+}
