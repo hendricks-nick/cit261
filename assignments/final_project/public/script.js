e28dd3ecf22cdc6b45b135ae8ef0ee147c86fd35
Nick Hendricks <hendricksn@byui.edu>
2020-03-28 11:03:39 -0600

    game screen

--- assignments/final_project/public/script.js
+++ assignments/final_project/public/script.js
@@ -1,5 +1,18 @@
 function loadHome() {
     console.log("Page Loaded");
+    let games = ["Match", "SquareClear", "Bubbles"];
+
+    document.getElementById("bodyContainer").innerHTML = "";
+    for(var i = 0; i < games.length; i++){
+        document.getElementById("bodyContainer").innerHTML += 
+        '<div class="gameContainer">'
+        '   <div class="gameTitle">' + games[i] + '</div>'
+        '   <div class="gameImage">'
+        '       <img src="/public/img/' + games[i].toLowerCase() + '.png" alt="' + games[i] + '" onclick="loadGames(\'' + games[i] + '\')"'
+        '   </div>'
+        '</div>';
+    }
+    
 }
 
 function menuChange(divObj) {
