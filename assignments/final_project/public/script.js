99b18edc8368d371c43d96f2a751c603988bcbd8
Nick Hendricks <hendricksn@byui.edu>
2020-03-29 10:15:18 -0600

    Match Game Done

--- assignments/final_project/public/script.js
+++ assignments/final_project/public/script.js
@@ -28,6 +28,13 @@ function loadGames(game){
 }
 
 // Match Game Functions
+class Card {
+    constructor(id, value) {
+        this.id = id;
+        this.value = value;
+    }
+}
+
 function matchGame(){
     document.getElementById("bodyContainer").innerHTML = "";
     
@@ -55,10 +62,59 @@ function matchGame(){
     for(var i = 0; i < 12; i++){
         document.getElementById("matchGameContainer").innerHTML +=
             '<div class="cardContainer">' +
-            '   <div class="cardImage" style="background-image: url(./public/img/card' + cardPlace[i] + '.png); background-repeat: no-repeat;width: 200px;height: 200px;">' +
+            '   <div class="cardImage" id="'+ i +'" value="' + cardPlace[i]  + '" onclick="flipCard(this)" style="background-image: url(./public/img/card' + cardPlace[i] + '.png); background-repeat: no-repeat;width: 200px;height: 200px;">' +
             '       <div class="cardCover"></div>' +
             '   </div>' +
             '</div>';
     }
+}
+
+var cardNum = 1;
+var card1;
+function flipCard(cardObj){
+    console.log("Flipping: " + cardObj.getAttribute('value'));
+    // Reveal card, stop it from being flipped back
+    cardObj.classList.toggle("flip");
+    cardObj.setAttribute('onclick', '');
+
+    // Create card object to store data to compare if match is made
+    if(cardNum === 1){
+        card1 = cardObj;
+        cardNum++;
+    }
+    else if (cardNum === 2){
+        
+        // Stops a third card or more from being flipped
+        document.getElementById('matchGameContainer').style.pointerEvents = 'none';
+        
+        // Delays flips and actions by 2 seconds
+        setTimeout(function(){
+            // MATCH
+            if(card1.getAttribute('value') === cardObj.getAttribute('value')){
+                card1.classList.toggle("flip");
+                card1.classList.toggle("found");
+                card1.childNodes[1].innerHTML += 
+                    '<i class="fas fa-check fa-9x"></i>';
+                card1.setAttribute('cursor', ''); 
+
+                cardObj.classList.toggle("flip");
+                cardObj.classList.toggle("found");
+                cardObj.childNodes[1].innerHTML += 
+                    '<i class="fas fa-check fa-9x"></i>';
+            }
+            // NO MATCH
+            else{
+                card1.classList.toggle("flip");
+                card1.setAttribute('onclick', 'flipCard(this)');
+
+                cardObj.classList.toggle("flip");
+                cardObj.setAttribute('onclick', 'flipCard(this)');
+            }
+            cardNum = 1;
+            document.getElementById('matchGameContainer').style.pointerEvents = '';
+        }, 2000);
+    }
+
+
 
 }
